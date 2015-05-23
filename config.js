6026780bdd01e12041056d7bc88eebd8ea99f607
PetaByteBoy <petabyteboy@gmail.com>
2015-05-23 19:14:38 +0200

    changes for gl.wupper

---
+++ config.js
@@ -0,0 +1,15 @@
+define({
+  "dataPath": "data/",
+  "siteName": "Freifunk gl.wupper",
+  "mapSigmaScale": 1.4,
+  "showContact": true,
+  "nodeInfos": [
+    { "name": "Clientstatistik",
+      "href": "data/nodes/{NODE_ID}.png",
+      "thumbnail": "data/nodes/{NODE_ID}.png",
+      "caption": "Knoten {NODE_ID}"
+    }
+  ],
+  "maxAge": 7
+})
+
